d197d585c5e013078b49c45d9b65a7c9093f6689
JYS <lk;lk>
2017-10-24 22:26:25 +0900

    ...

--- app/src/main/java/com/pie/herethere/MainActivity.java
+++ app/src/main/java/com/pie/herethere/MainActivity.java
@@ -1,15 +1,59 @@
 package com.pie.herethere;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ImageView img_1, img_2, img_3;
+
+    public void Declaration() {
+        viewPager = (ViewPager) findViewById(R.id.main_viewPager);
+
+        img_1 = (ImageView) findViewById(R.id.main_circle_1);
+        img_2 = (ImageView) findViewById(R.id.main_circle_2);
+        img_3 = (ImageView) findViewById(R.id.main_circle_3);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Declaration();
 
-        //
+        Main_ViewPager main_viewPager = new Main_ViewPager(getLayoutInflater());
+        viewPager.setAdapter(main_viewPager);
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0 :
+                        img_1.setImageResource(R.drawable.circle_1);
+                        img_2.setImageResource(R.drawable.circle_2);
+                        img_3.setImageResource(R.drawable.circle_2);
+                        break;
+                    case 1 :
+                        img_1.setImageResource(R.drawable.circle_2);
+                        img_2.setImageResource(R.drawable.circle_1);
+                        img_3.setImageResource(R.drawable.circle_2);
+                        break;
+                    case 2 :
+                        img_1.setImageResource(R.drawable.circle_2);
+                        img_2.setImageResource(R.drawable.circle_2);
+                        img_3.setImageResource(R.drawable.circle_1);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
     }
 }
---
+++ app/src/main/java/com/pie/herethere/Main_ViewPager.java
@@ -0,0 +1,49 @@
+package com.pie.herethere;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class Main_ViewPager extends PagerAdapter {
+
+    LayoutInflater inflater;
+    public Integer imgRe[] = new Integer[] {
+            R.drawable.red,
+            R.drawable.yello,
+            R.drawable.green
+    };
+
+    public Main_ViewPager(LayoutInflater inflater) {
+        this.inflater = inflater;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = null;
+        view = inflater.inflate(R.layout.main_viewpager, null);
+
+        ImageView img = (ImageView) view.findViewById(R.id.main_viewPager_img);
+
+        img.setImageResource(imgRe[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View)object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
