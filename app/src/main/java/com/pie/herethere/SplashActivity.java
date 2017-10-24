5478868886b37d0b3ef0e7992f0d5fadf391dcfa
yunjun-yeong <ypjun100@gmail.com>
2017-10-24 21:49:24 +0900

    윤준영 수정

---
+++ app/src/main/java/com/pie/herethere/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.pie.herethere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //1초후 메인화면으로 넘어감
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                Intent ite_main = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(ite_main);
+            }
+        }, 2000);
+    }
+}
