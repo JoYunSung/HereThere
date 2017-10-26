9193c514ad829bab674c0e9ae736b7e7c5ed1ca7
JYS <lk;lk>
2017-10-26 19:36:27 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/pie/herethere/SearchActivity.java

--- app/src/main/java/com/pie/herethere/AnyWhereActivity.java
+++ app/src/main/java/com/pie/herethere/AnyWhereActivity.java
@@ -1,7 +1,11 @@
 package com.pie.herethere;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class AnyWhereActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class AnyWhereActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_any_where);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("jua.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+    @Override
+    protected void attachBaseContext (Context newBase){
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
