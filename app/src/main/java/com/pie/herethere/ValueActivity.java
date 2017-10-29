34afb9750c4e28b2eae852c79b1dcc4c825eddcf
JYS <lk;lk>
2017-10-29 12:51:15 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/pie/herethere/SearchActivity.java

--- app/src/main/java/com/pie/herethere/ValueActivity.java
+++ app/src/main/java/com/pie/herethere/ValueActivity.java
@@ -7,8 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -20,11 +29,15 @@ public class ValueActivity extends AppCompatActivity implements View.OnClickList
 
     String contentId;
 
+    boolean isBookMarkOk = false;
+    ImageView bookMark;
+
     public void Declaration() {
         value_back = (ImageView) findViewById(R.id.value_back);
         value_back.setOnClickListener(this);
 
         value_img = (ImageView) findViewById(R.id.value_img);
+        bookMark = (ImageView) findViewById(R.id.value_book);
 
         value_toolbar = (TextView) findViewById(R.id.value_toolbar);
     }
@@ -50,6 +63,48 @@ public class ValueActivity extends AppCompatActivity implements View.OnClickList
                 .into(value_img);
 
         contentId = intent.getStringExtra("id");
+
+        try {
+            FileInputStream fis = openFileInput("Book-" + contentId + ".txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+
+            if (br.readLine().equals("북마크")) {
+                isBookMarkOk = true;
+                Toast.makeText(getApplicationContext(), "북마크 사용", Toast.LENGTH_SHORT).show();
+            }
+
+            br.close();
+            fis.close();
+        } catch (Exception e) { }
+
+        bookMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBookMarkOk) {
+                    // 북마크가 되어있을 시
+                    File file = new File("Book-" + contentId + ".txt");
+                    file.delete();
+
+                    isBookMarkOk = false;
+                    Toast.makeText(getApplicationContext(), "북마크 해제", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // 북마크가 되어있지않을 시
+                    try {
+                        FileOutputStream fo = openFileOutput("Book-" + contentId + ".txt", Context.MODE_PRIVATE);
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fo, "UTF-8"));
+
+                        bw.write("북마크");
+                        bw.close();
+                        fo.flush();
+                        fo.close();
+
+                        isBookMarkOk = true;
+                        Toast.makeText(getApplicationContext(), "북마크 사용", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) { }
+                }
+            }
+        });
     }
 
     @Override
