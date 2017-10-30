5c2b42657252c185ea2a729cc75b8c66a1a48772
JYS <lk;lk>
2017-10-30 21:22:43 +0900

    Merge branch 'master' of C:\Users\JYS\Desktop\HereThere with conflicts.

--- app/src/main/java/com/pie/herethere/WValueActivity.java
+++ app/src/main/java/com/pie/herethere/WValueActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -13,6 +14,8 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.pie.herethere.App.AppKey;
+import com.pie.herethere.Model.Forecast3day;
+import com.pie.herethere.Model.ForecastInfo;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -20,13 +23,20 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -39,8 +49,9 @@ public class WValueActivity extends AppCompatActivity implements View.OnClickLis
     String Title, Img, ContentId, lat, lon;
 
     AppKey appKey = new AppKey();
+    WValue_Service Service;
 
-    ListView ListView;
+    RecyclerView reView;
     WValue_ListAdapter adapter;
     ArrayList<Weather_ListData> listData = new ArrayList<>();
 
@@ -49,7 +60,7 @@ public class WValueActivity extends AppCompatActivity implements View.OnClickLis
         BackImage.setOnClickListener(this);
 
         BackGroundImg = (ImageView) findViewById(R.id.wvalue_img);
-        ListView = (ListView) findViewById(R.id.wvalue_list);
+        reView = (RecyclerView) findViewById(R.id.wvalue_recycler);
 
         ToolbarText = (TextView) findViewById(R.id.wvalue_toolbar);
     }
@@ -67,14 +78,15 @@ public class WValueActivity extends AppCompatActivity implements View.OnClickLis
         ToolbarText.setText(Title);
 
         ContentId = intent.getStringExtra("id");
-
+        Service = WValue_Service.retrofit.create(WValue_Service.class);
         try {
             URL url = new URL(appKey.getAppURL() + "searchKeyword?ServiceKey=" + appKey.getAppKey() + "&keyword=" + URLEncoder.encode(Title, "utf-8") +
                     "&areaCode=&sigunguCode=&cat1=&cat2=&cat3=&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=10&pageNo=1");
 
             Getlocation getlocation = new Getlocation();
             getlocation.execute(String.valueOf(url));
-        } catch (Exception e) { }
+        } catch (Exception e) {
+        }
     }
 
     @Override
@@ -108,6 +120,7 @@ public class WValueActivity extends AppCompatActivity implements View.OnClickLis
             }
             return document;
         }
+
         @Override
         protected void onPostExecute(Document document) {
             try {
@@ -136,9 +149,64 @@ public class WValueActivity extends AppCompatActivity implements View.OnClickLis
                     }
                 }
                 super.onPostExecute(document);
+            } catch (Exception e) {
             }
-            catch (Exception e) { }
         }
+    }
+
+    void getForecase3Days(final double latitude, final double longitude) {
+        Service.getForecase3Days(1, String.valueOf(latitude), String.valueOf(longitude)).enqueue(new Callback<ForecastInfo>() {
+            @Override
+            public void onResponse(Call<ForecastInfo> call, Response<ForecastInfo> response) {
+                if (response.body() == null)
+                    return;
+                ForecastInfo info = response.body();
+                if (info.weather == null || info.weather.forecast3days.isEmpty())
+                    return;
+
+                ArrayList<WValue_ListData> forecastInfo = new ArrayList<>();
+                int offsetHout = 3;
+                int maxHour = 49;
+
+                Date today = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("MM월 dd일 a hh시");
+
+                for (int hour = 4; hour <= maxHour; hour += offsetHout) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(today);
+                    calendar.add(Calendar.HOUR, hour);
+
+                    String time = sdf.format(calendar.getTime());
+                    String sky = "";
+                    String temp = "";
+
+                    try {
+                        Forecast3day forecast = info.weather.forecast3days.get(0);
+
+                        Field skyField = forecast.fcst3hour.sky.getClass().getField("name" + hour + "hour");
+                        sky = skyField.get(forecast.fcst3hour.sky).toString();
+
+                        Field tempField = forecast.fcst3hour.temperature.getClass().getField("temp" + hour + "hour");
+                        temp = tempField.get(forecast.fcst3hour.temperature).toString();
+
+                        if (temp.isEmpty() == false)
+                            temp = String.valueOf((int) Double.parseDouble(temp));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    forecastInfo.add(new WValue_ListData(lat, lon, sky, time));
+                }
+                reView.setHasFixedSize(true);
+
+
+                reView.setAdapter(adapter);
+
+                mForecastAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<ForecastInfo> call, Throwable t) { }
+        });
     }
 
     public void SetFont() {
--- app/src/main/java/com/pie/herethere/WValue_ListAdapter.java
+++ app/src/main/java/com/pie/herethere/WValue_ListAdapter.java
@@ -1,48 +1,49 @@
 package com.pie.herethere;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class WValue_ListAdapter extends BaseAdapter {
-    ArrayList<WValue_ListData> list;
-    LayoutInflater inflater;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    public WValue_ListAdapter(LayoutInflater inflater, ArrayList<WValue_ListData>list) {
-        this.list = list;
-        this.inflater = inflater;
+public class WValue_ListAdapter extends RecyclerView.Adapter {
+    private ArrayList<WValue_ListData> mDataset;
+
+    public WValue_ListAdapter(ArrayList<WValue_ListData>myDataset) {
+        mDataset = myDataset;
     }
 
-    @Override
-    public int getCount() {
-        return list.size();
-    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.wvalue_time)
+        TextView tvForeTime;
+        @BindView(R.id.wvalue_sky)
+        TextView tvForeSky;
 
-    @Override
-    public Object getItem(int i) {
-        return list.get(i).sky;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.wvalue_custom,null);
+        public ViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
         }
-        TextView time = (TextView) convertView.findViewById(R.id.wvalue_time);
-        time.setText(list.get(position).getTime());
+    }
 
-        TextView sky = (TextView) convertView.findViewById(R.id.wvalue_sky);
-        sky.setText(list.get(position).getSky());
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.wvalue_custom, parent, false);
+        return new ViewHolder(v);
+    }
 
-        return convertView;
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        holder.tvForeTime.setText(mDataset.get(position).time);
+        holder.tvForeSky.setText(mDataset.get(position).sky);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
     }
 }
